3413bf45ae5b5338a37909b7f57515fe57feb9fa
Yang, Lin <zcp914@linja.cof.ds.capitalone.com>
2015-10-26 14:18:01 -0700

    Initial Commit

---
+++ Tip_Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip_Calc
+//
+//  Created by Yang, Lin on 10/26/15.
+//  Copyright © 2015 Yang, Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
